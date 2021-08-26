b68858c952fffc658042e2b9fbd405e1ddd1a83b
argun8394 <argundoganali@gmail.com>
2021-08-26 22:59:15 +0300

    DOM

---
+++ DOM.js
@@ -0,0 +1,8 @@
+let greeting = document.querySelector("#greeting");
+
+greeting.addEventListener("click", domClick)
+
+function domClick() {
+    console.log("tıklandı")
+    this.style.color == "red" ? this.style.color = "blue" : this.style.color = "red";
+}

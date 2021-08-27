acdcc820a3d7c31f98e9e85a8d6d7b81d88befd6
argun8394 <argundoganali@gmail.com>
2021-08-27 16:11:37 +0300

    locale storage exam

---
+++ 024-localestorage.js
@@ -0,0 +1,28 @@
+// let word = "kodluyoruzz"
+
+// localStorage.setItem("wordInfo", word)
+
+
+
+// let user = { userName = "Ali", isActive = true }
+
+// localStorage.setItem("userInfo", JSON.stringify(user))
+
+// localStorage.getItem
+
+let counter = localStorage.getItem("counter") ? Number(localStorage.getItem("counter")) : 0;
+
+let counterDOM = document.querySelector("#counter");
+let increaseDOM = document.querySelector("#increase");
+let decreaseDOM = document.querySelector("#decrease");
+
+counterDOM.innerHTML = counter
+
+increaseDOM.addEventListener("click", clickEvent)
+decreaseDOM.addEventListener("click", clickEvent)
+
+function clickEvent() {
+    this.id == "increase" ? counter += 1 : counter -= 1;
+    localStorage.setItem("counter", counter)
+    counterDOM.innerHTML = counter
+};

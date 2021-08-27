27965709632a4cbe3334ab78885e6151a25cf2dd
argun8394 <argundoganali@gmail.com>
2021-08-27 23:54:33 +0300

    inputtan değer alma

--- 027-Form-submit.js
+++ 027-Form-submit.js
@@ -4,4 +4,8 @@ formDOM.addEventListener("submit", formSubmit);
 function formSubmit() {
     event.preventDefault(); //default işlemi engellemiş olduk
     console.log("işlem gerçekleştirildi")
+
+    let inputNameDOM = document.querySelector("#score");
+    console.log(inputNameDOM.value)
+    localStorage.setItem("input", inputNameDOM.value)
 }

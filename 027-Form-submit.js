ffcaf678c7c6ccba093343c54482dfb74344d019
argun8394 <argundoganali@gmail.com>
2021-08-31 22:17:28 +0300

    promise-await

--- 027-Form-submit.js
+++ 027-Form-submit.js
@@ -1,11 +1,35 @@
-let formDOM = document.querySelector("#userForm");
-formDOM.addEventListener("submit", formSubmit);
+// let formDOM = document.querySelector("#userForm");
+// formDOM.addEventListener("submit", formSubmit);
 
-function formSubmit() {
-    event.preventDefault(); //default işlemi engellemiş olduk
-    console.log("işlem gerçekleştirildi")
+// function formSubmit() {
+//     event.preventDefault(); //default işlemi engellemiş olduk
+//     console.log("işlem gerçekleştirildi")
 
-    let inputNameDOM = document.querySelector("#score");
-    console.log(inputNameDOM.value)
-    localStorage.setItem("input", inputNameDOM.value)
-}
+//     let inputNameDOM = document.querySelector("#score");
+//     console.log(inputNameDOM.value)
+//     localStorage.setItem("input", inputNameDOM.value)
+// }
+
+// async function getData() {
+//     const post1 = await (
+//         await fetch('https://jsonplaceholder.typicode.com/posts/1')
+//     ).json();
+
+
+//     const post2 = await (
+//         await fetch('https://jsonplaceholder.typicode.com/posts/1')
+//     ).json();
+
+
+//     const post3 = await (
+//         await fetch('https://jsonplaceholder.typicode.com/posts/1')
+//     ).json();
+
+
+//     console.log("post1", post1);
+//     console.log("post2", post2);
+//     console.log("post3", post3);
+
+// }
+
+// getData();
